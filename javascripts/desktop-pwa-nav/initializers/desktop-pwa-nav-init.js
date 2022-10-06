c1a72efb1cf92386063d5ed56d6ad2b868f628b0
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-10-06 10:58:14 +0200

    Add files via upload

---
+++ javascripts/desktop-pwa-nav/initializers/desktop-pwa-nav-init.js
@@ -0,0 +1,96 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "pwa-nav",
+
+  initialize(container) {
+    withPluginApi("0.8.13", (api) => {
+      const site = api.container.lookup("site:main");
+      this.capabilities = container.lookup("capabilities:main");
+      const isPwa = this.capabilities.isPwa;
+      const isIpadOS = this.capabilities.isIpadOS;
+      const isAndroid = this.capabilities.isAndroid;
+      const isWinphone = this.capabilities.isWinphone;
+      const isMobileDevice = site.isMobileDevice;
+      const mobileView = site.mobileView;
+
+      const isDesktop = isPwa && !isIpadOS && !isAndroid && !isWinphone && !isMobileDevice && !mobileView;
+
+      if (isDesktop) {
+        const applicationController = api.container.lookup("controller:application");
+        applicationController.set("showFooterNav", true);
+        document.body.classList.add("pwa-footer-nav");
+      }
+
+      api.reopenWidget("footer-nav", {
+        html(attrs) {
+          const buttons = [];
+
+          if (!isDesktop) {
+            buttons.push(
+              this.attach("flat-button", {
+                action: "goBack",
+                icon: "chevron-left",
+                className: "btn-large",
+                disabled: !attrs.canGoBack,
+                title: "footer_nav.back",
+              })
+            );
+
+            buttons.push(
+              this.attach("flat-button", {
+                action: "goForward",
+                icon: "chevron-right",
+                className: "btn-large",
+                disabled: !attrs.canGoForward,
+                title: "footer_nav.forward",
+              })
+            );
+          }
+
+          if (isDesktop) {
+            buttons.push(
+              this.attach("flat-button", {
+                action: "goBack",
+                icon: "chevron-left",
+                className: "btn-large",
+                title: "footer_nav.back",
+              })
+            );
+
+            buttons.push(
+              this.attach("flat-button", {
+                action: "goForward",
+                icon: "chevron-right",
+                className: "btn-large",
+                title: "footer_nav.forward",
+              })
+            );
+          }
+
+          if (this.capabilities.isAppWebview) {
+            buttons.push(
+              this.attach("flat-button", {
+                action: "share",
+                icon: "link",
+                className: "btn-large",
+                title: "footer_nav.share",
+              })
+            );
+
+            buttons.push(
+              this.attach("flat-button", {
+                action: "dismiss",
+                icon: "chevron-down",
+                className: "btn-large",
+                title: "footer_nav.dismiss",
+              })
+            );
+          }
+
+          return buttons;
+        },
+      });
+    });
+  },
+};
